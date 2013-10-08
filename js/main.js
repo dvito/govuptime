29c533df092247543dde07f2a21f5237d5be760e
GSMcNamara <holler@incless.com>
2013-10-08 14:52:37 -0400

    Reduced horizontal bar chart to only department size by total employees under normal conditions, and changed color so as to not conflict with donut charts.

--- js/main.js
+++ js/main.js
@@ -4,18 +4,19 @@ function chart_this(){
   $.getJSON("js/employees.json",function(data){
     employee_data = data;
     array_data = [];
-    array_data.push(["Department", "Employees", "Furloughed"]);
-    sorted_for_total = _.sortBy(employee_data, function(item){ return item.furloughed}).reverse();
+    array_data.push(["Department", "Employees"]);
+    sorted_for_total = _.sortBy(employee_data, function(item){ return item.employees}).reverse();
     $.each(sorted_for_total, function(i, item) {
-      array_data.push([item.short_name,item.employees,item.furloughed]);
+      array_data.push([item.short_name,item.employees]);
     });
     var data = google.visualization.arrayToDataTable(array_data);
 
     var options = {
-      title: 'Furloughed Employees (Logarithmic Scale)',
-      vAxis: {title: 'Department',  titleTextStyle: {color: 'red'}},
+      title: 'Total Employees (Logarithmic Scale)',
+      vAxis: {title: 'Department',  titleTextStyle: {color: 'black'}},
       height: 800,
-      hAxis: { logScale: true }
+      hAxis: { logScale: true },
+      colors:['slategray']
     };
 
     var chart = new google.visualization.BarChart(document.getElementById('employees_chart'));
