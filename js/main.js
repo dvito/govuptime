c447e5273aefb479b5f0a56b1baa4722ca92ef75
justraines@gmail.com <justraines@gmail.com>
2013-10-09 13:30:14 -0400

    move hours.js into main.js and compress, also added number formatting to tooltips

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-google.load("visualization", "1", {packages:["corechart"]});
+google.load("visualization", "1", {packages:["corechart"], 'language': 'ja'});
 google.setOnLoadCallback(chart_this);
 function chart_this(){
   $.getJSON("js/employees.json",function(data){
     employee_data = data;
     array_data = [];
-    array_data.push(["Department", "Employees"]);
     sorted_for_total = _.sortBy(employee_data, function(item){ return item.employees}).reverse();
     $.each(sorted_for_total, function(i, item) {
-      array_data.push([item.short_name,item.employees]);
+      console.log(item);
+      array_data.push([item.short_name,item.employees,""+item.name+" employees: "+item.employees.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")]);
     });
-    var data = google.visualization.arrayToDataTable(array_data);
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Department');
+    data.addColumn('number', 'Employees')
+    data.addColumn({type: 'string', role: 'tooltip'});
+    data.addRows(array_data);
 
     var options = {
       title: 'Total Employees (Logarithmic Scale)',
@@ -25,4 +29,40 @@ function chart_this(){
       var $spy = $(this).scrollspy('refresh')
     });
   });
+}
+
+function workingDaysBetweenDates(startDate, endDate) {
+
+    // Validate input
+    if (endDate < startDate)
+        return 0;
+
+    // Calculate days between dates
+    var millisecondsPerDay = 86400 * 1000; // Day in milliseconds
+    startDate.setHours(0,0,0,1);  // Start just after midnight
+    endDate.setHours(23,59,59,999);  // End just before midnight
+    var diff = endDate - startDate;  // Milliseconds between datetime objects
+    var days = Math.ceil(diff / millisecondsPerDay);
+
+    // Subtract two weekend days for every week in between
+    var weeks = Math.floor(days / 7);
+    var days = days - (weeks * 2);
+
+    // Handle special cases
+    var startDay = startDate.getDay();
+    var endDay = endDate.getDay();
+
+    // Remove weekend not previously removed.
+    if (startDay - endDay > 1)
+        days = days - 2;
+
+    // Remove start day if span starts on Sunday but ends before Saturday
+    if (startDay == 0 && endDay != 6)
+        days = days - 1
+
+    // Remove end day if span ends on Saturday but starts after Sunday
+    if (endDay == 6 && startDay != 0)
+        days = days - 1
+
+    return days;
 }
